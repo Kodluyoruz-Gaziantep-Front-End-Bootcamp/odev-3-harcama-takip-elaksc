114a4bcfed00a6a3d94fef1f625a45b7dfd36874
Ela Kaşcıoglu <77547429+elaksc@users.noreply.github.com>
2022-08-28 17:22:42 +0300

    Update ExpenseTracker.js

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -2,20 +2,14 @@ import React, { useState, useEffect } from "react";
 import Expense from "./Expense";
 import TransactionHistory from "./History";
 import TransactionForm from "./Form";
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-];
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
 
 function ExpenseTracker() {
   const [expense, setExpense] = useState(0);
   const [income, setIncome] = useState(0);
   const [transactions, setTransactions] = useState([]);
+
 
 
   const saveState = () => {
@@ -72,7 +66,6 @@ function ExpenseTracker() {
       />
       <TransactionForm onNewTransaction={handleAddNewTransaction} />
 
-     
     </div>
   );
 }
